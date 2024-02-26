fc30b9acac22cedc4cad98a242000052d171e6b6
tejeoner34 <atzarco37@gmail.com>
2024-02-26 20:06:10 +0100

    replace scrollview by flatlist

--- src/screens/MealDetail.js
+++ src/screens/MealDetail.js
@@ -1,35 +1,43 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { MEALS } from '../../data/data';
 import MealItem from '../components/MealItem';
+
+function renderMealitem(itemData) {
+  const { item: meal } = itemData;
+  const mealItemProps = {
+    affordability: meal.affordability,
+    complexity: meal.complexity,
+    title: meal.title,
+    duration: meal.duration,
+    imageUrl: meal.imageUrl,
+  };
+  return (
+    <View style={styles.itemContainer}>
+      <MealItem {...mealItemProps} />
+    </View>
+  );
+}
 
 export default function MealDetail({ route }) {
   const { categoryId } = route.params;
   const mealsToDisplay = MEALS.filter((meal) => meal.categoryIds.includes(categoryId));
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {mealsToDisplay.map((meal) => (
-        <View style={styles.itemContainer} key={meal.id}>
-          <MealItem
-            affordability={meal.affordability}
-            complexity={meal.complexity}
-            title={meal.title}
-            duration={meal.duration}
-            imageUrl={meal.imageUrl}
-          />
-        </View>
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        data={mealsToDisplay}
+        renderItem={renderMealitem}
+        keyExtractor={(meal) => meal.id}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   itemContainer: {
-    width: '90%',
-    paddingBottom: 24,
+    padding: 10,
   },
 });
